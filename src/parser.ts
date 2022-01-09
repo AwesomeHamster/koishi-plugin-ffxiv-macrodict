b1cebddecc8b90ec2007c1e0a00d8ee08c0929f5
Maiko Tan <maiko.tan.coding@gmail.com>
2022-01-09 12:04:36 +0000

    feat: add controller key style

--- src/parser.ts
+++ src/parser.ts
@@ -1,3 +1,16 @@
+const keyMap: { [k: string]: string} = {
+  "1": "↑",
+  "2": "↓",
+  "3": "←",
+  "4": "→",
+  "8": "〇",
+  "9": "X",
+  "10": "□",
+  "11": "△",
+  "14": "L2",
+  "15": "R2",
+};
+
 /**
  * Parse a macro definition text for HTML.
  */
@@ -18,6 +31,10 @@ export function parseMacroDescriptionForHtml(description: string): string {
       /<UIForeground>F201FA<\/UIForeground><UIGlow>F201FB<\/UIGlow>(.*?)<UIGlow>01<\/UIGlow><UIForeground>01<\/UIForeground>/g,
       "<span class='highlight'>$1</span>",
     )
+    // replace <Gui(x)/> to corresponding keys
+    .replace(/<Gui\((\d+)\)\/>/g, (_match, key) => {
+      return `<kbd class="key">${keyMap[key] || key}</kbd>`;
+    })
     .replace(/<(\w+)>/g, "&lt;$1&gt;")
     .replace(/\n/g, "<br>");
 }
