513ad748ee6c7704854a7f07794ee0666784ae9b
Maiko Sinkyaet Tan <maiko.tan.coding@gmail.com>
2024-04-01 13:59:11 +0800

    refactor: remove aliases and update i18n for schema (#56)
    * refactor: remove aliases and update i18n for schema
    
    * fix: several lint errors

--- src/config.ts
+++ src/config.ts
@@ -3,15 +3,19 @@ import { Schema } from 'koishi'
 import { Locale, locales } from './utils'
 
 export interface Config {
-  aliases: string[]
   defaultLanguage: Locale
   defaultMode: 'auto' | 'text'
   threshold: number
 }
 
 export const Config: Schema<Config> = Schema.object({
-  aliases: Schema.array(Schema.string()).default([]),
   defaultLanguage: Schema.union(locales).default('en'),
-  defaultMode: Schema.union(['auto', 'text'] as const).default('auto'),
+  defaultMode: Schema.union([Schema.const('auto'), Schema.const('text')])
+    .role('radio')
+    .default('auto'),
   threshold: Schema.number().default(3),
+}).i18n({
+  'zh': require('./locales/zh-CN/macrodict.schema.yml'),
+  'zh-CN': require('./locales/zh-CN/macrodict.schema.yml'),
+  'en': require('./locales/en-US/macrodict.schema.yml'),
 })
---
+++ src/locales/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { Context } from 'koishi'
+
+export function apply(ctx: Context) {
+  ctx.i18n.define('en', require('./en-US/macrodict.yml'))
+  ctx.i18n.define('zh', require('./zh-CN/macrodict.yml'))
+  ctx.i18n.define('zh-CN', require('./zh-CN/macrodict.yml'))
+}
